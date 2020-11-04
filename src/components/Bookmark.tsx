39b41dbe2236bbeed08c3f56588e82df151f4972
achenson <45999022+Achenson@users.noreply.github.com>
2020-11-04 15:20:43 +0100

    bookmark svgs init

---
+++ src/components/Bookmark.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { ReactComponent as PaintSVG } from "../svgs/paint-roller.svg";
+import { ReactComponent as ColorSVG } from "../svgs/beaker.svg";
+import { ReactComponent as ColorSmallSVG } from "../svgs/beakerSmall.svg";
+import { ReactComponent as PencilSVG } from "../svgs/pencil.svg";
+import { ReactComponent as PencilSmallSVG } from "../svgs/pencilSmall.svg";
+import { ReactComponent as TrashSVG } from "../svgs/trash.svg";
+import { ReactComponent as TrashSmallSVG } from "../svgs/trashSmall.svg";
+import { ReactComponent as HandSVG } from "../svgs/hand.svg";
+import { ReactComponent as HandSmallSVG } from "../svgs/handSmall.svg";
+import { ReactComponent as CrossArrowsSVG } from "../svgs/cross-arrows.svg";
+
+interface Props {}
+
+function Bookmark({}: Props): JSX.Element {
+  return (
+    <div className="h-8 px-2 pt-px bg-teal-600 border border-gray-500 shadow-sm flex justify-between cursor-pointer">
+      <div>Bookmark</div>
+      <div className="pt-1 flex opacity-0 hover:opacity-100">
+        <CrossArrowsSVG className="h-6 ml-2 cursor-move" style={{marginTop: "-2px"}}/>
+        <PencilSmallSVG className="h-5 ml-2"/>
+        <ColorSmallSVG className="h-5 ml-2" />
+        <TrashSmallSVG className="h-5 ml-2"/>
+      </div>
+    </div>
+  );
+}
+
+export default Bookmark;
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
+import React from "react";
+import Bookmark from "./Bookmark";
 
-interface Props {
+interface Props {}
 
+function Grid({}: Props): JSX.Element {
+  return (
+    <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 mx-4">
+      <div className="bg-yellow-200">
+        <Bookmark/>  1
+      </div>
+      <div className="hidden sm:block bg-orange-200">2</div>
+      <div className="hidden md:block bg-red-200">3</div>
+      <div className="hidden lg:block bg-green-200">4</div>
+    </div>
+  );
 }
-
-function Grid({}: Props): JSX.Element{
-    return (
-
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 mx-4">
-
-          <div className="bg-yellow-200">1</div>
-          <div className="hidden sm:block bg-orange-200">2</div>
-          <div className="hidden md:block bg-red-200">3</div>
-          <div className="hidden lg:block bg-green-200">4</div>
-        </div>
-    )
-};
 
 export default Grid;
