8e9efbdc2fbb8b4b0e78f047a5ec49dc36c05a1a
Achenson <ach.01.mail@gmail.com>
2020-11-07 16:29:55 +0100

    colors changeable for each bookmark

--- src/components/SingleColor.tsx
+++ src/components/SingleColor.tsx
@@ -10,13 +10,21 @@ interface Props {
 function SingleColor({ color, bookmarkTitle }: Props): JSX.Element {
   const [bookmarksData, setBookmarksData] = bookmarksDataState.use();
 
+  let bookmarkIndex: number;
+
+  bookmarksData.forEach( (obj, i) => {
+    if (obj.title === bookmarkTitle) {
+      bookmarkIndex = i
+    }
+  })
+
   return (
     <div
       className={`h-4 w-8 bg-${color} cursor-pointer border border-black hover:border-gray-500`}
       onClick={() => {
         setBookmarksData((previous) =>
           produce(previous, (updated) => {
-            updated[0].color = `bg-${color}`;
+            updated[bookmarkIndex].color = `bg-${color}`;
           })
         );
       }}
