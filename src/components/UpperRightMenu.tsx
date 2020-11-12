4ce5125b2043b1bcf2d5f1990b072be2c3bd2c77
achenson <45999022+Achenson@users.noreply.github.com>
2020-11-12 13:44:42 +0100

    UpperRightMenu with icons

--- src/components/UpperRightMenu.tsx
+++ src/components/UpperRightMenu.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from "react";
 
-interface Props {
-    
-}
+import { ReactComponent as AddFolderSVG } from "../svgs/addFolder.svg";
+import { ReactComponent as AddLinkSVG } from "../svgs/addLink.svg";
+import { ReactComponent as SettingsSVG } from "../svgs/settingsAlt.svg";
+import { ReactComponent as UserSVG } from "../svgs/user.svg";
 
-function UpperRightMenu({}: Props): JSX.Element{
-    return (
-        <div className="bg-pink-400 h-10 w-32 absolute right-0 bottom-0 mb-2">
-           Upper Right Menu
-        </div>
-    )
+interface Props {}
+
+function UpperRightMenu({}: Props): JSX.Element {
+  return (
+    <div className=" h-10 w-40 absolute right-0 bottom-0 mb-2 flex justify-around items-center">
+      <AddFolderSVG className="h-6" />
+      <AddLinkSVG className="h-6" />
+      <SettingsSVG className="h-6"/>
+      <UserSVG className="h-6"/>
+    </div>
+  );
 }
 
 export default UpperRightMenu;
