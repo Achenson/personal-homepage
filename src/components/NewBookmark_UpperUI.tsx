1b523e72aba0482808ee101cf896b82fbebb74cd
achenson <45999022+Achenson@users.noreply.github.com>
2020-11-19 10:36:40 +0100

    NewBookmark_UpperUI working

--- src/components/NewBookmark_UpperUI.tsx
+++ src/components/NewBookmark_UpperUI.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 import { useState } from "react";
 
@@ -10,19 +10,17 @@ import { bookmarksDataState } from "../state/bookmarksAndLinks";
 import { linksDataState } from "../state/bookmarksAndLinks";
 
 interface Props {
-  setNewBookmarkVis: React.Dispatch<React.SetStateAction<boolean>>
+  setNewBookmarkVis: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-function NewBookmark_UpperUI({setNewBookmarkVis}: Props): JSX.Element{
-
+function NewBookmark_UpperUI({ setNewBookmarkVis }: Props): JSX.Element {
   const [bookmarksData, setBookmarksData] = bookmarksDataState.use();
   const [linksData, setLinksData] = linksDataState.use();
 
-  const [bookmarkTitleInput, setBookmarkTitleInput] = useState<string>("")
-  const [bookmarkLinksInput, setBookmarkLinksInput] = useState<string[]>([])
-  const [bookmarkColumnInput, setBookmarkColumnInput] = useState<number>(
-   1
-  );
+  const [bookmarkTitleInput, setBookmarkTitleInput] = useState<string>("");
+
+  const [bookmarkColumnInput, setBookmarkColumnInput] = useState<number>(1);
+  const [bookmarkLinksInput, setBookmarkLinksInput] = useState<string[]>([]);
 
   // let bookmarkIndex: number;
 
@@ -36,74 +34,62 @@ function NewBookmark_UpperUI({setNewBookmarkVis}: Props): JSX.Element{
 
   let regexForTitle = /^\w+$/;
 
-
-
-    return (
-          // opacity cannot be used, because children will inherit it and the text won't be readable
+  return (
+    // opacity cannot be used, because children will inherit it and the text won't be readable
     <div
-    className="flex z-50 absolute h-screen w-screen items-center justify-center"
-    style={{ backgroundColor: "rgba(90, 90, 90, 0.4)" }}
-  >
-    <div
-      className="bg-gray-200 pb-3 pt-6 border-2 border-teal-500 rounded-sm md:mb-48"
-      style={{ width: "350px" }}
+      className="flex z-50 absolute h-screen w-screen items-center justify-center"
+      style={{ backgroundColor: "rgba(90, 90, 90, 0.4)" }}
     >
-      <form action="" className="pl-2 pr-4">
-        <div className="flex justify-around mb-2 mt-2">
-          <p className="w-32">Title</p>
-          <div className="w-full pl-2">
-            <input
-              type="text"
-              className="w-full border border-gray-500"
-              value={""}
-              placeholder={"new folder title"}
-              onChange={(e) => setBookmarkTitleInput(e.target.value)}
-            />
-        
+      <div
+        className="bg-gray-200 pb-3 pt-6 border-2 border-teal-500 rounded-sm md:mb-48"
+        style={{ width: "350px" }}
+      >
+        <form action="" className="pl-2 pr-4">
+          <div className="flex justify-around mb-2 mt-2">
+            <p className="w-32">Title</p>
+            <div className="w-full pl-2">
+              <input
+                type="text"
+                className="w-full border border-gray-500"
+                value={bookmarkTitleInput}
+                placeholder={"new folder title"}
+                onChange={(e) => setBookmarkTitleInput(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="flex justify-around mb-2 mt-2">
+            <p className="w-32">Column</p>
+            <div className="w-full pl-2">
+              <input
+                type="number"
+                min="1"
+                max="4"
+                className="w-full border border-gray-500"
+                value={bookmarkColumnInput}
+                onChange={(e) =>
+                  setBookmarkColumnInput(parseInt(e.target.value))
+                }
+                placeholder={"Enter number between 1 and 4"}
+              />
+            </div>
           </div>
 
-
-        </div>
-        <div className="flex justify-around mb-2 mt-2">
-          <p className="w-32">Column</p>
-          <div className="w-full pl-2">
-        
-            <input type="number"
-            min="1"
-            max="4"
-              className="w-full border border-gray-500"
-              value={bookmarkColumnInput}
-              onChange={(e) => setBookmarkColumnInput(parseInt(e.target.value))}
-
-            placeholder={"Enter number between 1 and 4"}
-            />
+          <div className="flex justify-around mb-2 mt-2">
+            <p className="w-32">Bookmarks</p>
+            <div className="w-full pl-2">
+              <input
+                type="text"
+                className="w-full border border-gray-500"
+                value={bookmarkLinksInput.join(", ")}
+                onChange={(e) =>
+                  setBookmarkLinksInput([...e.target.value.split(", ")])
+                }
+                placeholder={"Choose at least one from existing bookmarks"}
+              />
+            </div>
           </div>
 
-          
-        </div>
-
-        <div className="flex justify-around mb-2 mt-2">
-          <p className="w-32">Bookmarks</p>
-          <div className="w-full pl-2">
-        
-            <input type="text"
-          
-              className="w-full border border-gray-500"
-              value={bookmarkColumnInput}
-              onChange={(e) => setBookmarkColumnInput(parseInt(e.target.value))}
-
-            placeholder={"Choose at least one from existing bookmarks"}
-            />
-          </div>
-
-          
-        </div>
-
-
-
-
-
-        {/* <div className="flex justify-around mb-2">
+          {/* <div className="flex justify-around mb-2">
           <p className="w-10">Link</p>
           <div className="w-full pl-2">
             <input
@@ -118,7 +104,7 @@ function NewBookmark_UpperUI({setNewBookmarkVis}: Props): JSX.Element{
 
         </div> */}
 
-        {/* <div className="flex justify-start mb-2">
+          {/* <div className="flex justify-start mb-2">
           <p className="w-10">Tags</p>
           <div className="w-full pl-2">
             <input
@@ -131,13 +117,13 @@ function NewBookmark_UpperUI({setNewBookmarkVis}: Props): JSX.Element{
           </div>
         </div> */}
 
-        {/* {titleFormatErrorVis ? (
+          {/* {titleFormatErrorVis ? (
           <p className={`text-red-600`}>
             Link title can contain letters, numbers or underscore
           </p>
         ) : null} */}
 
-        {/* {titleUniquenessErrorVis ? (
+          {/* {titleUniquenessErrorVis ? (
           <p className={`text-red-600`}>
             Link with that title already exists
           </p>
@@ -153,99 +139,128 @@ function NewBookmark_UpperUI({setNewBookmarkVis}: Props): JSX.Element{
           <p className={`text-red-600`}>Each tag should be unique</p>
         ) : null} */}
 
-        <div className="flex justify-start mt-3">
-          <p className="w-8"></p>
-          {/* !!! pl-4 in NewLink */}
-          <div className="w-full flex justify-center">
-            <button
-              onClick={(e) => {
-                e.preventDefault();
+          <div className="flex justify-start mt-3">
+            <p className="w-8"></p>
+            {/* !!! pl-4 in NewLink */}
+            <div className="w-full flex justify-center">
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
 
-                // if(tagsInput.join(", "))
+                  // if(tagsInput.join(", "))
 
-                // setTagErrorVis(false);
-                // setTagRepeatErrorVis(false);
-                // setTitleFormatErrorVis(false);
-                // setTitleUniquenessErrorVis(false);
+                  // setTagErrorVis(false);
+                  // setTagRepeatErrorVis(false);
+                  // setTitleFormatErrorVis(false);
+                  // setTitleUniquenessErrorVis(false);
 
-                // if (!regexForTitle.test(titleInput)) {
-                //   setTitleFormatErrorVis(true);
+                  // if (!regexForTitle.test(titleInput)) {
+                  //   setTitleFormatErrorVis(true);
 
-                //   return;
-                // }
+                  //   return;
+                  // }
 
-                // if (!titleUniquenessCheck()) {
-                //   setTitleUniquenessErrorVis(true);
-                //   return;
-                // }
+                  // if (!titleUniquenessCheck()) {
+                  //   setTitleUniquenessErrorVis(true);
+                  //   return;
+                  // }
 
-                // if (!regexForTags.test(tagsInput.join(", "))) {
-                //   setTagErrorVis(true);
-                //   return;
-                // }
+                  // if (!regexForTags.test(tagsInput.join(", "))) {
+                  //   setTagErrorVis(true);
+                  //   return;
+                  // }
 
-                // if (!tagUniquenessCheck()) {
-                //   setTagRepeatErrorVis(true);
-                //   return;
-                // }
+                  // if (!tagUniquenessCheck()) {
+                  //   setTagRepeatErrorVis(true);
+                  //   return;
+                  // }
 
-                // setLinksData((previous) =>
-                //   produce(previous, (updated) => {
-                //     updated.push({
-                //       title: titleInput,
-                //       URL: urlInput,
-                //       tags: [...tagsInput],
-                //     });
-                //   })
-                // );
+                  // setLinksData((previous) =>
+                  //   produce(previous, (updated) => {
+                  //     updated.push({
+                  //       title: titleInput,
+                  //       URL: urlInput,
+                  //       tags: [...tagsInput],
+                  //     });
+                  //   })
+                  // );
 
-                // setNewLinkVis((b) => !b);
+                  // setNewLinkVis((b) => !b);
 
-                // function tagUniquenessCheck() {
-                //   let isUnique: boolean = true;
+                  // function tagUniquenessCheck() {
+                  //   let isUnique: boolean = true;
 
-                //   tagsInput.forEach((el, i) => {
-                //     let tagsInputCopy = [...tagsInput];
-                //     tagsInputCopy.splice(i, 1);
+                  //   tagsInput.forEach((el, i) => {
+                  //     let tagsInputCopy = [...tagsInput];
+                  //     tagsInputCopy.splice(i, 1);
 
-                //     if (tagsInputCopy.indexOf(el) > -1) {
-                //       isUnique = false;
-                //       return;
-                //     }
-                //   });
+                  //     if (tagsInputCopy.indexOf(el) > -1) {
+                  //       isUnique = false;
+                  //       return;
+                  //     }
+                  //   });
 
-                //   return isUnique;
-                // }
+                  //   return isUnique;
+                  // }
 
-              //   function titleUniquenessCheck() {
-              //     let isUnique: boolean = true;
+                  //   function titleUniquenessCheck() {
+                  //     let isUnique: boolean = true;
 
-              //     linksData.forEach((obj, i) => {
-              //       if (obj.title === titleInput) {
-              //         isUnique = false;
-              //       }
-              //     });
+                  //     linksData.forEach((obj, i) => {
+                  //       if (obj.title === titleInput) {
+                  //         isUnique = false;
+                  //       }
+                  //     });
 
-              //     return isUnique;
-              //   }
-              }}
-            >
-              <SaveSVG className="h-5 fill-current text-black mr-3 hover:text-green-600" />
-            </button>
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                setNewBookmarkVis((b) => !b);
-              }}
-            >
-              <CancelSVG className="h-5 fill-current text-black ml-3 hover:text-red-600" />
-            </button>
+                  //     return isUnique;
+                  //   }
+                }}
+              >
+                <SaveSVG
+                  className="h-5 fill-current text-black mr-3 hover:text-green-600"
+                  onClick={(e) => {
+                    e.preventDefault();
+
+                    setBookmarksData((previous) =>
+                      produce(previous, (updated) => {
+                        updated.push({
+                          title: bookmarkTitleInput,
+                          column: bookmarkColumnInput,
+                          color: "bg-teal-400",
+                          priority: 0,
+                        });
+                      })
+                    );
+
+                    setLinksData((previous) =>
+                      produce(previous, (updated) => {
+                        updated.forEach((obj) => {
+                          if (
+                            bookmarkLinksInput.indexOf(obj.title) > -1 &&
+                            obj.tags.indexOf(bookmarkTitleInput) === -1
+                          ) {
+                            obj.tags.push(bookmarkTitleInput);
+                          }
+                        });
+                      })
+                    );
+                  }}
+                />
+              </button>
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  setNewBookmarkVis((b) => !b);
+                }}
+              >
+                <CancelSVG className="h-5 fill-current text-black ml-3 hover:text-red-600" />
+              </button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
-  </div>
-    )
-};
+  );
+}
 
 export default NewBookmark_UpperUI;
