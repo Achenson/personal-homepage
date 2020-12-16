2d31028a4ca31d70cdf483adf08e8819a285f0a3
achenson <45999022+Achenson@users.noreply.github.com>
2020-12-16 16:36:51 +0100

    columns color final2

--- src/components/ColorsToChoose.tsx
+++ src/components/ColorsToChoose.tsx
@@ -7,31 +7,6 @@ interface Props {
 }
 
 const colors = [
-  // ],
-  // [
-  //   "warmGray-200",
-  //   "warmGray-300",
-  //   "warmGray-400",
-  //   // "warmGray-500",
-  //   // "warmGray-600",
-  //   "warmGray-700",
-  //   "warmGray-800",
-  //   "warmGray-900",
-  //   //  "gray-500",
-  //   // "black",
-  // ],
-
-  // [
-  //   "gray-300",
-  //   "blueGray-300",
-  //   "blueGray-400",
-  //   // "blueGray-500",
-  //   "gray-500",
-  //   "blueGray-600",
-  //   "blueGray-700",
-  //   "gray-800",
-  // ],
-
   [
     "blueGray-303",
     // "blueGray-300",
@@ -52,10 +27,6 @@ const colors = [
     "amber-400",
     "amber-500",
     "amber-600",
-
-    // "amber-700",
-
-    // "yellow-330",
   ],
   [
     "yellow-303",
@@ -74,9 +45,6 @@ const colors = [
     "orange-600",
     "orange-700",
     "orange-800",
-
-    // "orange-440",
-    // "orange-660",
   ],
   ["red-404", "orange-404", "orange-606", "red-606", "amber-700", "amber-900"],
   [
@@ -86,11 +54,7 @@ const colors = [
     "red-600",
     "red-700",
     "red-800",
-
-    // "red-440",
-    // "red-660",
   ],
-  // ["rose-300", "rose-400", "rose-500", "rose-600", "rose-700", "rose-800"],
   [
     "rose-300",
     "rose-400",
@@ -98,20 +62,8 @@ const colors = [
     "rose-600",
     "rose-700",
     "rose-800",
-    // "pink-300",
-    // "pink-400",
-    // "pink-600",
   ],
 
-  // ["lime-300", "lime-400", "lime-500", "lime-600", "lime-700", "lime-800"],
-  // [
-  //   "green-300",
-  //   "green-400",
-  //   "green-500",
-  //   "green-600",
-  //   "green-700",
-  //   "green-800",
-  // ],
   [
     "emerald-300",
     "emerald-400",
@@ -119,8 +71,6 @@ const colors = [
     "emerald-600",
     "emerald-700",
     "emerald-800",
-    // "green-400",
-    // "green-500",
   ],
   [
     "teal-300",
@@ -129,10 +79,7 @@ const colors = [
     "teal-600",
     "teal-700",
     "teal-800",
-    // "lime-300",
-    // "lime-400",
   ],
-  // ["cyan-300", "cyan-400", "cyan-500", "cyan-600", "cyan-700", "cyan-800"],
   [
     "cyan-300",
     "cyan-400",
@@ -140,18 +87,7 @@ const colors = [
     "cyan-600",
     "cyan-700",
     "cyan-800",
-    // "lightBlue-400",
-    // "lightBlue-600",
   ],
-  // [
-  //   "lightBlue-300",
-  //   "lightBlue-400",
-  //   "lightBlue-500",
-  //   "lightBlue-600",
-  //   "lightBlue-700",
-  //   "lightBlue-800",
-  // ],
-
   [
     "blue-300",
     "blue-400",
@@ -159,44 +95,7 @@ const colors = [
     "blue-600",
     "blue-700",
     "blue-800",
-    // "lightBlue-700",
-    // "blue-770",
   ],
-  // [
-  //   "indigo-300",
-  //   "indigo-400",
-  //   "indigo-500",
-  //   "indigo-600",
-  //   "indigo-700",
-  //   "indigo-800",
-  //   // "violet-600",
-  //   // "violet-700",
-  // ],
-
-  // [
-  //   "violet-300",
-  //   "violet-400",
-  //   "violet-500",
-  //   "violet-600",
-  //   "violet-700",
-  //   "violet-800",
-  // ],
-  // [
-  //   "purple-300",
-  //   "purple-400",
-  //   "purple-500",
-  //   "purple-600",
-  //   "purple-700",
-  //   "purple-800",
-  // ],
-  // [
-  //   "fuchsia-300",
-  //   "fuchsia-400",
-  //   "fuchsia-500",
-  //   "fuchsia-600",
-  //   "fuchsia-700",
-  //   "fuchsia-800",
-  // ],
   [
     "lightBlue-400",
     "lightBlue-600",
@@ -204,22 +103,14 @@ const colors = [
     "blue-707",
     "blue-770",
     "blue-777",
-    // "violet-600",
-    // "violet-700",
   ],
   [
-    // "purple-300",
-    // "fuchsia-400",
     "pink-300",
     "pink-400",
     "pink-600",
     "indigo-400",
     "indigo-600",
     "indigo-800",
-
-    // "fuchsia-400",
-    // "fuchsia-600",
-    // "fuchsia-700",
   ],
   [
     "fuchsia-400",
@@ -231,7 +122,6 @@ const colors = [
     // "purple-700",
     "purple-800",
   ],
-  // ["white", "white", "white", "white", "white", "white"],
 ];
 
 // const colorsBackUp = [
