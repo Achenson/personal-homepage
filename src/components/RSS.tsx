5b64d2bcf85b8832415df7d44239de8d10914cee
Achenson <ach.01.mail@gmail.com>
2020-12-29 15:40:39 +0100

    rss-parser working

--- src/components/RSS.tsx
+++ src/components/RSS.tsx
@@ -1,33 +1,51 @@
 import React from 'react'
 
+let Parser = require('rss-parser');
+let parser = new Parser();
+
 interface Props {
    rssLink: string | null
 }
 
+
+
 function RSS({rssLink}: Props): JSX.Element{
 
-    fetchRSS(rssLink as string);
+    // fetchRSS(rssLink as string);
 
-    function fetchRSS(rssLink : string) {
-        // fetch(rssLink, {
-        //     method: "GET",
-        //     // mode: 'no-cors',
-        //     // headers: {
-        //     //   'Access-Control-Allow-Origin':'*'
-        //     // }
-        // }).then( res => res.text()).then( data => console.log(data)
-        // )
+//     function fetchRSS(rssLink : string) {
+//         // fetch(rssLink, {
+//         //     method: "GET",
+//         //     // mode: 'no-cors',
+//         //     // headers: {
+//         //     //   'Access-Control-Allow-Origin':'*'
+//         //     // }
+//         // }).then( res => res.text()).then( data => console.log(data)
+//         // )
 
-        fetch(rssLink, {
-            // method: "GET",
-            // mode: "no-cors"
-        })
-  .then(response => response.text())
-  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-  .then(data => console.log(data))
+//         fetch(rssLink, {
+//             // method: "GET",
+//             // mode: "no-cors"
+//         })
+//   .then(response => response.text())
+//   .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+//   .then(data => console.log(data))
 
 
-    }
+//     }
+
+(async () => {
+
+    // let feed = await parser.parseURL('https://www.reddit.com/.rss');
+    let feed = await parser.parseURL(rssLink);
+    console.log(feed.title);
+  
+  // @ts-ignore: Unreachable code error
+    feed.items.forEach(item => {
+      console.log(item.title + ':' + item.link)
+    });
+  
+  })();
 
 
     return (
