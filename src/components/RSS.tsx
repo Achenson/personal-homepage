938cc79bfcc89f153759ea9da10423963e1099dd
Achenson <ach.01.mail@gmail.com>
2021-01-01 19:53:31 +0100

    SingleRssNews initial styling

--- src/components/RSS.tsx
+++ src/components/RSS.tsx
@@ -9,7 +9,6 @@ let Parser = require("rss-parser");
 let parser = new Parser();
 
 interface Props {
-  //  rssLink: string | null
   bookmarkID: string | number;
   bookmarkIndex: number;
 }
@@ -18,77 +17,51 @@ function RSS({ bookmarkID, bookmarkIndex }: Props): JSX.Element {
   const [bookmarksData, setBookmarksData] = bookmarksDataState.use();
   const [toDisplay, setToDisplay] = useState([
     {
-      title: "sth",
-      link: "someLink",
+      title: "loading data...",
+      link: "loading data...",
+      pubDate: "loading data..."
     },
   ]);
-  // const [toDisplay, setToDisplay] = useState("df");
-
-  // let bookmarkIndex: number;
-
-  // bookmarksData.forEach((obj, i) => {
-  //   if (obj.id === bookmarkID) {
-  //     bookmarkIndex = i;
-  //   }
-  // });
 
   useEffect(() => {
     let currentBookmark = bookmarksData.filter((obj) => obj.id === bookmarkID);
 
     // @ts-ignore: Unreachable code error
-    if (toDisplay[0].title === "sth") {
-      parser
-        .parseURL(currentBookmark[0].rssLink)
+    if (toDisplay[0].title === "loading data...") {
+      parser.parseURL(currentBookmark[0].rssLink).then(
         // @ts-ignore: Unreachable code error
-        .then(
+        (feed) => {
           // @ts-ignore: Unreachable code error
-          (feed) => {
-            // feed.items.forEach((item) => console.log(item.title + " : " + item.link))
+          // feed.items.forEach((item) =>
+          //   console.log(item.title + " : " + item.link + " " + item.pubDate)
+          // );
 
-            // @ts-ignore: Unreachable code error
-            // feed.items.forEach((item) =>
-            //   console.log(item.title + " : " + item.link + " " + item.pubDate)
-            // );
+          setBookmarksData((previous) =>
+            produce(previous, (updated) => {
+              // @ts-ignore: Unreachable code error
+              updated[bookmarkIndex].items.push({
+                ...feed.items[0],
+              });
+            })
+          );
 
-            // console.log(feed.items[0])
-            setBookmarksData((previous) =>
-              produce(previous, (updated) => {
-                // @ts-ignore: Unreachable code error
-                updated[bookmarkIndex].items.push({
-                  ...feed.items[0],
-                });
-              })
-            );
-
-            // setToDisplay(feed.items[0].title)
-
-            setToDisplay((previous) =>
-              produce(previous, (updated) => {
-                // updated.splice(0, updated.length)
-
-                if (toDisplay.length === 1) {
-                  for (let i = 0; i < 3; i++) {
-                    updated.push(feed.items[i]);
-                  }
+          setToDisplay((previous) =>
+            produce(previous, (updated) => {
+              if (toDisplay.length === 1) {
+                updated.shift();
+                for (let i = 0; i < 3; i++) {
+                  updated.push(feed.items[i]);
                 }
-
-                // updated.shift();
-              })
-            );
-
-            // let arrOfObj = [];
-
-            // for (let i = 0; i < 3; i++) {
-            //   arrOfObj.push(feed.items[i]);
-            // }
-            // // @ts-ignore: Unreachable code error
-            // setToDisplay([...arrOfObj]);
-          }
-        );
+              }
+            })
+          );
+        }
+      );
     } else {
       setToDisplay((previous) =>
         produce(previous, (updated) => {
           if (toDisplay.length === 1) {
+            updated.shift();
             for (let i = 0; i < 3; i++) {
               // @ts-ignore: Unreachable code error
               updated.push(bookmarksData[bookmarkIndex].items[i]);
@@ -96,18 +69,6 @@ function RSS({ bookmarkID, bookmarkIndex }: Props): JSX.Element {
           }
         })
       );
-
-      // let arrOfObj = [];
-
-      // for (let i = 0; i < 3; i++) {
-      //   // @ts-ignore: Unreachable code error
-      //   arrOfObj.push(bookmarksData[bookmarkIndex].items[i]);
-      // }
-      // // @ts-ignore: Unreachable code error
-      // setToDisplay([...arrOfObj]);
-
-      // @ts-ignore: Unreachable code error
-      // setToDisplay(bookmarksData[bookmarkIndex].items[0].title)
     }
 
     console.log(toDisplay[0].title);
@@ -115,26 +76,13 @@ function RSS({ bookmarkID, bookmarkIndex }: Props): JSX.Element {
     // console.log(bookmarksData[bookmarkIndex].items);
   }, [bookmarksData, setBookmarksData, bookmarkIndex, bookmarkID, toDisplay]);
 
-  // (async () => {
-  //   // let feed = await parser.parseURL('https://www.reddit.com/.rss');
-  //   let feed = await parser.parseURL(currentBookmark[0].rssLink);
-  //   // console.log(feed.title);
-
-  //   // @ts-ignore: Unreachable code error
-  //   feed.items.forEach((item) => {
-  //     console.log(item.title + ":" + item.link);
-  //   });
-  // })();
-
   return (
     <div>
       {toDisplay.map((el, i) => {
-        return <SingleRssNews title={el.title} link={el.link} key={i} />;
+        return <SingleRssNews title={el.title} link={el.link} key={i} pubDate={el.pubDate} />;
       })}
     </div>
   );
-
-  // return <div>null</div>;
 }
 
 export default RSS;
--- src/components/SingleRssNews.tsx
+++ src/components/SingleRssNews.tsx
@@ -1,27 +1,32 @@
-import React from 'react'
-
-import { bookmarksDataState } from "../state/bookmarksAndLinks";
-
+import React from "react";
 
 interface Props {
-    title: string,
-    link: string
-    
+  title: string;
+  link: string;
+  pubDate: string;
 }
 
-function SingeRssNews({title, link}: Props): JSX.Element{
+function SingeRssNews({ title, link, pubDate }: Props): JSX.Element {
+  if (title === "loading data...") {
+    return <div>{title}</div>;
+  }
+  // const [bookmarksData, setBookmarksData] = bookmarksDataState.use();
 
+  // let currentBookmark = bookmarksData.filter((obj) => obj.id === bookmarkID);
 
-    // const [bookmarksData, setBookmarksData] = bookmarksDataState.use();
-
-    // let currentBookmark = bookmarksData.filter((obj) => obj.id === bookmarkID);
-
-    return (
-        <div>
-         <p>{title}</p>
-         <p>{link}</p>
-        </div>
-    )
+  return (
+    <div className="bg-gray-50 py-1 px-2 border-b">
+      <a
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:underline text-lightBlue-700 "
+      >
+        {title}
+      </a>
+      <p>{pubDate}</p>
+    </div>
+  );
 }
 
 export default SingeRssNews;
