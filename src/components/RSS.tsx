5fc2675f2dd50ea27d4757a2431d5a1cba779bdc
Achenson <ach.01.mail@gmail.com>
2021-01-02 17:30:00 +0100

    RSS 10 out of 50 pages displayed

--- src/components/RSS.tsx
+++ src/components/RSS.tsx
@@ -19,9 +19,30 @@ function RSS({ bookmarkID, bookmarkIndex }: Props): JSX.Element {
     {
       title: "loading data...",
       link: "loading data...",
-      pubDate: "loading data..."
+      pubDate: "loading data...",
     },
   ]);
+  const itemsPerPage = 10;
+  const [pageNumber, setPageNumber] = useState(0);
+  const [currentPage, setCurrentPage] = useState(
+    calcCurrentPage(toDisplay, pageNumber, itemsPerPage)
+  );
+
+  function calcCurrentPage(
+    toDisplay: any,
+    pageNumber: number,
+    itemsPerPage: number
+  ) {
+    let arrOfObj = [];
+
+    for (let i = 0 + (pageNumber*10); i < itemsPerPage + (pageNumber*10); i++) {
+      if (toDisplay[i]) {
+        arrOfObj.push(toDisplay[i]);
+      }
+    }
+
+    return arrOfObj;
+  }
 
   useEffect(() => {
     let currentBookmark = bookmarksData.filter((obj) => obj.id === bookmarkID);
@@ -36,12 +57,26 @@ function RSS({ bookmarkID, bookmarkIndex }: Props): JSX.Element {
           //   console.log(item.title + " : " + item.link + " " + item.pubDate)
           // );
 
+          let howManyItems: number;
+
+          if (feed.length < 50) {
+            howManyItems = feed.length;
+          } else {
+            howManyItems = 50;
+          }
+
           setBookmarksData((previous) =>
             produce(previous, (updated) => {
               // @ts-ignore: Unreachable code error
-              updated[bookmarkIndex].items.push({
-                ...feed.items[0],
-              });
+              // updated[bookmarkIndex].items.push({
+              //   ...feed.items[0],
+              // });
+              for (let i = 0; i < howManyItems; i++) {
+                // @ts-ignore: Unreachable code error
+                updated[bookmarkIndex].items.push({
+                  ...feed.items[i],
+                });
+              }
             })
           );
 
@@ -49,7 +84,7 @@ function RSS({ bookmarkID, bookmarkIndex }: Props): JSX.Element {
             produce(previous, (updated) => {
               if (toDisplay.length === 1) {
                 updated.shift();
-                for (let i = 0; i < 3; i++) {
+                for (let i = 0; i < howManyItems; i++) {
                   updated.push(feed.items[i]);
                 }
               }
@@ -62,7 +97,8 @@ function RSS({ bookmarkID, bookmarkIndex }: Props): JSX.Element {
         produce(previous, (updated) => {
           if (toDisplay.length === 1) {
             updated.shift();
-            for (let i = 0; i < 3; i++) {
+            // @ts-ignore: Unreachable code error
+            for (let i = 0; i < bookmarksData[bookmarkIndex].items?.length; i++) {
               // @ts-ignore: Unreachable code error
               updated.push(bookmarksData[bookmarkIndex].items[i]);
             }
@@ -71,15 +107,31 @@ function RSS({ bookmarkID, bookmarkIndex }: Props): JSX.Element {
       );
     }
 
+    setCurrentPage(calcCurrentPage(toDisplay, pageNumber, itemsPerPage));
     console.log(toDisplay[0].title);
 
     // console.log(bookmarksData[bookmarkIndex].items);
-  }, [bookmarksData, setBookmarksData, bookmarkIndex, bookmarkID, toDisplay]);
+  }, [
+    bookmarksData,
+    setBookmarksData,
+    bookmarkIndex,
+    bookmarkID,
+    toDisplay,
+    pageNumber,
+  ]);
 
   return (
     <div>
-      {toDisplay.map((el, i) => {
-        return <SingleRssNews title={el.title} link={el.link} key={i} pubDate={el.pubDate} />;
+      {/* {toDisplay.map((el, i) => { */}
+      {currentPage.map((el, i) => {
+        return (
+          <SingleRssNews
+            title={el.title}
+            link={el.link}
+            key={i}
+            pubDate={el.pubDate}
+          />
+        );
       })}
     </div>
   );
