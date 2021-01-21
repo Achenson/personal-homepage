07539b4657154eb9c915f0b731b373acdbcf962b
achenson <45999022+Achenson@users.noreply.github.com>
2021-01-21 15:55:55 +0100

    background color change svgIcon final

--- src/components/Colors/BackgroundColor.tsx
+++ src/components/Colors/BackgroundColor.tsx
@@ -5,30 +5,50 @@ import { backgroundColorState } from "../../state/colorsState";
 import { ReactComponent as DocumentSVG } from "../../svgs/document.svg";
 
 interface Props {
-  setBackgroundColorsToChooseVis: React.Dispatch<React.SetStateAction<boolean>>
+  setBackgroundColorsToChooseVis: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-function BackgroundColor({setBackgroundColorsToChooseVis}: Props): JSX.Element {
+function BackgroundColor({
+  setBackgroundColorsToChooseVis,
+}: Props): JSX.Element {
   const [
     backgroundColorData,
     setBackgroundColorState,
   ] = backgroundColorState.use();
 
- const [selected, setSelected] = useState(false)
+  const [selected, setSelected] = useState(false);
 
+  function calcIconBackground(pageBackgroundColor: string) {
+    if (pageBackgroundColor === "white") {
+      return `$bg-${pageBackgroundColor}`;
+    }
 
+    if (pageBackgroundColor === "black") {
+      return `bg-white fill-current text-${backgroundColorData}`;
+    }
+
+    let whiteRegex = /[3456789]00$/;
+
+    if (whiteRegex.test(pageBackgroundColor)) {
+      return `bg-white fill-current text-${backgroundColorData}`;
+    } else {
+      return `$bg-${pageBackgroundColor}`;
+    }
+  }
 
   return (
     // <div className={`h-8 w-6 bg-${backgroundColorData} ${selected ? "border-2" : "border"} border-black cursor-pointer hover:border-gray-400`}
-  
-      <DocumentSVG className={`h-7 bg-gray-100 border border-black rounded-lg cursor-pointer fill-current text-${backgroundColorData} hover:border-gray-500`}
-    onClick={ () => {
-      setSelected(b => !b)
-      setBackgroundColorsToChooseVis(b=>!b)
-    }}
+
+    <DocumentSVG
+      // className={`h-7 bg-${calcIconBackground(backgroundColorData)} opacity-80 border border-black rounded-lg cursor-pointer fill-current text-${backgroundColorData} hover:border-gray-500`}
+      className={`h-7 ${calcIconBackground(
+        backgroundColorData
+      )} opacity-80 border border-black rounded-lg cursor-pointer  hover:border-gray-500`}
+      onClick={() => {
+        setSelected((b) => !b);
+        setBackgroundColorsToChooseVis((b) => !b);
+      }}
     />
-  
-  
   );
 }
 
