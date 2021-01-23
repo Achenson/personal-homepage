83b25c0a13853f9ae5f8b4714356783b902da292
Achenson <ach.01.mail@gmail.com>
2021-01-23 16:44:43 +0100

    RSS reactQuery version initial

--- src/components/RSS_reactQuery.tsx
+++ src/components/RSS_reactQuery.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import SingleRssNews from "./SingleRssNews";
 
 import { bookmarksDataState } from "../state/bookmarksAndLinks";
+
+import { ReactComponent as ArrowLeft } from "../svgs/arrowLeft.svg";
+import { ReactComponent as ArrowRight } from "../svgs/arrowRight.svg";
 
 let Parser = require("rss-parser");
 let parser = new Parser();
@@ -14,6 +17,9 @@ interface Props {
 function ReactQuery({ bookmarkID }: Props): JSX.Element {
   const [bookmarksData, setBookmarksData] = bookmarksDataState.use();
 
+  const itemsPerPage = 10;
+  const [pageNumber, setPageNumber] = useState(0);
+
   const { data, status } = useQuery("feed", fetchFeed, {
     // staleTime: 2000,
     // cacheTime: 10,
@@ -23,9 +29,28 @@ function ReactQuery({ bookmarkID }: Props): JSX.Element {
 
   async function fetchFeed() {
     let currentBookmark = bookmarksData.filter((obj) => obj.id === bookmarkID);
-    const response = await parser.parseURL(currentBookmark[0].rssLink)
+    const response = await parser.parseURL(currentBookmark[0].rssLink);
 
     return response;
+  }
+
+  function mapData() {
+    let arrOfObj = [];
+
+    for (let i = 0 + pageNumber * 10; i < itemsPerPage + pageNumber * 10; i++) {
+      if (data.items[i]) {
+        arrOfObj.push(data.items[i]);
+      }
+    }
+
+    return arrOfObj.map((el, i) => (
+      <SingleRssNews
+        title={el.title}
+        link={el.link}
+        key={i}
+        pubDate={el.pubDate}
+      />
+    ));
   }
 
   return (
@@ -34,21 +59,34 @@ function ReactQuery({ bookmarkID }: Props): JSX.Element {
 
       {status === "error" && <div>Error fetching data</div>}
 
-      {status === "success" && (
-        <div>
-          {
-            // @ts-ignore: Unreachable code error
-            data.items.map((el, i) => (
-              <SingleRssNews
-                title={el.title}
-                link={el.link}
-                key={i}
-                pubDate={el.pubDate}
-              />
-            ))
-          }
-        </div>
-      )}
+      {status === "success" && <div>{mapData()}</div>}
+
+      <div className="flex bg-gray-50 justify-end ">
+        <ArrowLeft
+          className={`h-8 ${
+            pageNumber === 0
+              ? `text-gray-400`
+              : `cursor-pointer text-black hover:bg-gray-200`
+          }`}
+          onClick={() => {
+            if (pageNumber > 0) {
+              setPageNumber(pageNumber - 1);
+            }
+          }}
+        />
+        <ArrowRight
+          className={`h-8 ${
+            pageNumber === 4
+              ? `text-gray-400`
+              : `cursor-pointer text-black hover:bg-gray-200`
+          }`}
+          onClick={() => {
+            if (pageNumber < 4) {
+              setPageNumber(pageNumber + 1);
+            }
+          }}
+        />
+      </div>
     </div>
   );
 }
