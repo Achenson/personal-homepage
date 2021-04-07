817286c4da386562f1b85d2aab61d62620ee399e
Achenson <ach.01.mail@gmail.com>
2021-04-07 12:45:44 +0200

    title text for new Bookmark Tab RSS Note

--- src/components/UpperUI/NewBookmark_UpperUI.tsx
+++ src/components/UpperUI/NewBookmark_UpperUI.tsx
@@ -111,10 +111,11 @@ Props): JSX.Element {
       style={{ backgroundColor: "rgba(90, 90, 90, 0.4)" }}
     >
       <div
-        className="bg-gray-200 pb-2 pt-6 pl-2 pr-1 border-2 border-teal-500 rounded-sm md:mb-48"
+        className="bg-gray-200 pb-2 pt-3 pl-2 pr-1 border-2 border-teal-500 rounded-sm md:mb-48"
         style={{ width: "350px" }}
       >
-        <div className="flex justify-around mb-2 mt-2">
+          <p className="text-center">New bookmark</p>
+        <div className="flex justify-around mb-2 mt-3">
           <p className="w-10">Title</p>
 
           <input
--- src/components/UpperUI/NewTab_UpperUI.tsx
+++ src/components/UpperUI/NewTab_UpperUI.tsx
@@ -171,10 +171,11 @@ function NewTab_UpperUI({ tabType, upperVisDispatch }: Props): JSX.Element {
       style={{ backgroundColor: "rgba(90, 90, 90, 0.4)" }}
     >
       <div
-        className="bg-gray-200 pb-2 pt-6 pl-2 pr-1 border-2 border-teal-500 rounded-sm md:mb-48"
+        className="bg-gray-200 pb-2 pt-3 pl-2 pr-1 border-2 border-teal-500 rounded-sm md:mb-48"
         style={{ width: "350px" }}
       >
-        <div className="flex justify-around mb-2 mt-2">
+        <p className="text-center">New {tabType === "folder" ? "folder" : tabType === "note" ? "note" : "RSS channel"}</p>
+        <div className="flex justify-around mb-2 mt-3">
           <p className="w-32">Title</p>
           {/* <div className="w-full pl-2"> */}
           <input
