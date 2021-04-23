0033a71196bc6b80826286ff76023bfc59a06fc8
Achenson <ach.01.mail@gmail.com>
2021-04-23 15:15:49 +0200

    xs new svgs

--- src/components/UpperUI/UpperRightMenu.tsx
+++ src/components/UpperUI/UpperRightMenu.tsx
@@ -3,11 +3,18 @@ import React from "react";
 import { ReactComponent as AddFolderSVG } from "../../svgs/addFolder.svg";
 import { ReactComponent as FolderSVG } from "../../svgs/folder.svg";
 
+
+import { ReactComponent as PlusSmSVG } from "../../svgs/plus-sm.svg";
+import { ReactComponent as PlusSVG } from "../../svgs/plus.svg";
+import { ReactComponent as PlusSquareSVG } from "../../svgs/plus-square-line_uxwing.svg";
+import { ReactComponent as PlusCircleSVG } from "../../svgs/plus-circle.svg";
+import { ReactComponent as PlusCircleZondSVG } from "../../svgs/add-outline_zond.svg";
 import { ReactComponent as BookmarkSVG } from "../../svgs/bookmarkAlt.svg";
 // import { ReactComponent as AddNote } from "../../svgs/addNote.svg";
 import { ReactComponent as AddNote } from "../../svgs/text-document-add.svg";
 import { ReactComponent as NoteSVG } from "../../svgs/note_UXwing.svg";
 import { ReactComponent as SettingsSVG } from "../../svgs/settingsAlt.svg";
+import { ReactComponent as CogSVG } from "../../svgs/cog.svg";
 import { ReactComponent as UserSVG } from "../../svgs/user.svg";
 import { ReactComponent as ColorSVG } from "../../svgs/beaker.svg";
 import { ReactComponent as AddRssSVG } from "../../svgs/rss.svg";
@@ -38,10 +45,10 @@ function UpperRightMenu({
 
   return (
     // <div className=" h-10 w-56 absolute right-0 bottom-0 mb-2 flex justify-between items-center">
-    <div className="relative h-7 w-28 xs:w-56  flex justify-between items-center bg-white bg-opacity-80 rounded-md border border-gray-700 "
+    <div className="h-7 w-28 xs:w-56 flex justify-between items-center bg-white bg-opacity-80 rounded-md border border-gray-700 "
     style={{marginBottom: "2px"}}
     >
-      <div className="flex w-28 justify-around">
+      <div className="hidden xs:flex w-28 justify-around">
         {/* <AddLinkSVG */}
         <BookmarkSVG
           className={`h-7 cursor-pointer transition-colors duration-75 hover:text-${uiColorData}`}
@@ -86,6 +93,58 @@ function UpperRightMenu({
         />
       </div>
 
+{/* XS============================== */}
+
+      <div className="flex xs:hidden w-28 justify-around">
+        {/* <AddLinkSVG */}
+        <BookmarkSVG
+          className={`h-7 cursor-pointer transition-colors duration-75 hover:text-${uiColorData}`}
+          onClick={() => {
+            
+            // setNewBookmarkVis((b) => !b);
+            upperVisDispatch({type: "NEW_BOOKMARK_TOGGLE"})
+          }}
+        />
+
+        {/* <AddFolderSVG */}
+        <PlusSquareSVG
+          className={`h-5 w-5 self-center cursor-pointer transition-colors duration-75 fill-current hover:text-${uiColorData} mr-1`}
+          onClick={() => {
+            
+            // setNewTabVis((b) => !b);
+            upperVisDispatch({type: "NEW_TAB_TOGGLE"})
+            setTabType("folder");
+          }}
+        />
+        {/* <AddNote */}
+        <CogSVG
+          className={`h-6 cursor-pointer transition-colors duration-75  hover:text-${uiColorData}`}
+          style={{marginTop: "2px"}}
+          onClick={() => {
+            // setNewTabVis((b) => !b);
+           
+            upperVisDispatch({type: "NEW_TAB_TOGGLE"})
+            setTabType("note");
+          }}
+        />
+
+        {/* <AddRssSVG className={`h-6 cursor-pointer hover:text-${uiColorData}`} */}
+        <UserSVG
+          className={`h-6 self-center cursor-pointer transition-colors duration-75 hover:text-${uiColorData}`}
+          onClick={() => {
+            // setNewTabVis((b) => !b);
+            
+            upperVisDispatch({type: "NEW_TAB_TOGGLE"})
+            setTabType("rss");
+          }}
+        />
+      </div>
+
+
+
+
+
+
 
 
 
@@ -122,37 +181,7 @@ function UpperRightMenu({
       </div>
 
 
-      <div className="absolute flex xs:hidden h-7 w-28 justify-around items-center bg-white bg-opacity-80 rounded-md border border-gray-700"
-       style={{marginBottom: "2px", bottom: "26px", left: "-1px"}}
-      >
-        <PhotographSVG
-          className={`h-6 cursor-pointer transition-colors duration-75 hover:text-${uiColorData}`}
-          onClick={() => {
-            // setBackgroundSettingsVis((b) => !b);
-         
-            upperVisDispatch({type: "BACKGROUND_SETTINGS_TOGGLE"})
-          }}
-        />
-        <ColorSVG
-          className={`h-6 cursor-pointer transition-colors duration-75 hover:text-${uiColorData}`}
-          onClick={() => {
-           
-            upperVisDispatch({type: "COLORS_SETTINGS_TOGGLE"})
-            // setColorsVis((b) => !b);
-          }}
-        />
-        <SettingsSVG
-          className={`h-6 cursor-pointer transition-colors duration-75 hover:text-${uiColorData}`}
-          onClick={() => {
-
-            // setTabOpenedData(null)
-
-        // setCloseAllTabsData(true);
-            upperVisDispatch({type: "SETTINGS_TOGGLE"})
-          }}
-        />
-        <UserSVG className="h-6" style={{marginLeft: "-3px"}} />
-      </div>
+      
 
 
 
