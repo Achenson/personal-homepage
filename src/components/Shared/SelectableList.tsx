f37a5c01d3431cab064b0a9a0b96d0c69cfe60ae
achenson <45999022+Achenson@users.noreply.github.com>
2021-04-29 13:55:46 +0200

    SelectableList wasAnythingChanged bug fixed

--- src/components/Shared/SelectableList.tsx
+++ src/components/Shared/SelectableList.tsx
@@ -1,14 +1,10 @@
 import React, { useState, useEffect } from "react";
 
-
-
-
-
 interface Props {
   selectablesInputStr: string;
   setSelectablesInputStr: React.Dispatch<React.SetStateAction<string>>;
   visibleSelectables: string[];
-  
+
   marginTop: string;
   setWasAnythingClicked?: React.Dispatch<React.SetStateAction<boolean>>;
 }
@@ -17,19 +13,19 @@ function SelectableList({
   setSelectablesInputStr,
   selectablesInputStr,
   visibleSelectables,
-  
-  marginTop,
-  setWasAnythingClicked
-}: Props): JSX.Element {
 
-  let visibleSelectables_sorted = visibleSelectables.sort()
-  
+  marginTop,
+  setWasAnythingClicked,
+}: Props): JSX.Element {
+  let visibleSelectables_sorted = visibleSelectables.sort();
+
+  let selectableToHighlight: null | number = null;
 
   return (
     <div
       className="absolute z-50 bg-white w-full border border-t-0"
       // style={{ width: "271px", marginLeft: "42px" }}
-      style={{  marginTop: marginTop }}
+      style={{ marginTop: marginTop }}
     >
       {visibleSelectables_sorted.length === 0 ? (
         <p className="invisible">[empty]</p>
@@ -39,13 +35,15 @@ function SelectableList({
             <p
               className="cursor-pointer hover:bg-blueGray-200 pl-px truncate"
               onClick={() => {
+                if (setWasAnythingClicked) {
+                  (setWasAnythingClicked as React.Dispatch<
+                    React.SetStateAction<boolean>
+                  >)(true);
+                }
+
                 if (selectablesInputStr.length === 0) {
                   setSelectablesInputStr(el);
                   return;
-                }
-
-                if(setWasAnythingClicked) {
-                  (setWasAnythingClicked as React.Dispatch<React.SetStateAction<boolean>>)(true);
                 }
 
                 setSelectablesInputStr(selectablesInputStr.concat(", " + el));
@@ -57,8 +55,6 @@ function SelectableList({
           );
         })
       )}
-
-    
     </div>
   );
 }
