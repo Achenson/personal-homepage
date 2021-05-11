3a8c973a35dc09bcf0f04f35ada0c2fc5e86ef6f
achenson <45999022+Achenson@users.noreply.github.com>
2021-05-11 10:53:03 +0200

    notes bg changed to amber 100

--- src/components/LowerUI/NoteInput.tsx
+++ src/components/LowerUI/NoteInput.tsx
@@ -4,7 +4,6 @@ import { tabsDataState } from "../../state/tabsAndBookmarks";
 import { SingleTabData, TabVisAction } from "../../utils/interfaces";
 import { globalSettingsState } from "../../state/defaultSettings";
 
-
 interface Props {
   //  noteInput: string | null;
   // setEditTabVis: React.Dispatch<React.SetStateAction<boolean>>;
@@ -12,17 +11,23 @@ interface Props {
   currentTab: SingleTabData;
 }
 
-function NoteInput({visDispatch, currentTab }: Props): JSX.Element {
+function NoteInput({ visDispatch, currentTab }: Props): JSX.Element {
   const [tabsData, setTabsData] = tabsDataState.use();
   const [globalSettingsData, setGlobalSettingsData] = globalSettingsState.use();
 
   // let currentTab = tabsData.filter((obj) => obj.id === tabID);
 
   return (
-    <div className={`${globalSettingsData.picBackground ? "bg-gray-100 bg-opacity-20 border-b border-gray-800 border-opacity-10" : "bg-yellow-50 border border-t-0"} p-2 `}>
+    <div
+      className={`${
+        globalSettingsData.picBackground
+          ? "bg-gray-100 bg-opacity-20 border-b border-gray-800 border-opacity-10"
+          : "bg-amber-100 border border-black border-opacity-10 border-t-0"
+      } p-2 `}
+    >
       <div
         className="p-2 rounded-md overflow-hidden border border-black border-opacity-10"
-        style={{backgroundColor: "rgb(247, 243, 132)"}}
+        style={{ backgroundColor: "rgb(247, 243, 132)" }}
         onClick={() => {
           // setEditTabVis(true);
           visDispatch({ type: "EDIT_TOGGLE" });
