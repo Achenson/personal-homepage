d59662b8f412fd82e1d6a4b793b3ba10f6cc83f4
achenson <45999022+Achenson@users.noreply.github.com>
2021-07-20 13:23:55 +0200

    bookmarksData, tabsData

---
+++ src/state/bookmarksData.ts
@@ -0,0 +1,39 @@
+import {SingleBookmarkData} from "../utils/interfaces"
+
+export const bookmarksData: SingleBookmarkData[] = [
+  {
+    id: 1,
+    title: "facebook",
+    URL: "https://en.wikipedia.org/wiki/Deadly_Rooms_of_Death",
+    // tags: ["all", "fun"],
+    tags: ["ALL_TAGS", "3"],
+  },
+  {
+    id: 2,
+    title: "tvn24 d",
+    URL: "https://en.wikipedia.org/wiki/Webfoot_Technologies",
+    // tags: ["all", "main"],
+    tags: ["ALL_TAGS", "2"],
+  },
+  {
+    id: 3,
+    title: "gmail",
+    URL: "https://en.wikipedia.org/wiki/Microsoft_Windows",
+    // tags: ["all", "main"],
+    tags: ["ALL_TAGS", "2"],
+  },
+  {
+    id: 4,
+    title: "oneMore",
+    URL: "https://en.wikipedia.org/wiki/Microsoft_Windows",
+    // tags: ["all", "main"],
+    tags: ["ALL_TAGS"],
+  },
+  {
+    id: 5,
+    title: "anotherOne",
+    URL: "https://en.wikipedia.org/wiki/Microsoft_Windows",
+    // tags: ["all", "main"],
+    tags: ["ALL_TAGS"],
+  },
+]
--- src/state/useBookmarks.ts
+++ src/state/useBookmarks.ts
@@ -4,6 +4,7 @@ import create from "zustand";
 import produce from "immer";
 
 import { SingleBookmarkData } from "../utils/interfaces";
+import { bookmarksData } from "./bookmarksData";
 
 interface UseBookmarks {
   addBookmark: (singleBookmarkData: SingleBookmarkData) => void;
@@ -189,43 +190,9 @@ export const useBookmarks = create<UseBookmarks>((set, get) => ({
     }));
   },
   bookmarksAllTags: ["ALL_TAGS", "2", "3"],
-  bookmarks: [
-    {
-      id: 1,
-      title: "facebook",
-      URL: "https://en.wikipedia.org/wiki/Deadly_Rooms_of_Death",
-      // tags: ["all", "fun"],
-      tags: ["ALL_TAGS", "3"],
-    },
-    {
-      id: 2,
-      title: "tvn24 d",
-      URL: "https://en.wikipedia.org/wiki/Webfoot_Technologies",
-      // tags: ["all", "main"],
-      tags: ["ALL_TAGS", "2"],
-    },
-    {
-      id: 3,
-      title: "gmail",
-      URL: "https://en.wikipedia.org/wiki/Microsoft_Windows",
-      // tags: ["all", "main"],
-      tags: ["ALL_TAGS", "2"],
-    },
-    {
-      id: 4,
-      title: "oneMore",
-      URL: "https://en.wikipedia.org/wiki/Microsoft_Windows",
-      // tags: ["all", "main"],
-      tags: ["ALL_TAGS"],
-    },
-    {
-      id: 5,
-      title: "anotherOne",
-      URL: "https://en.wikipedia.org/wiki/Microsoft_Windows",
-      // tags: ["all", "main"],
-      tags: ["ALL_TAGS"],
-    },
-  ],
+  bookmarks: [...bookmarksData],
+
+ 
 }));
 
 export const bookmarksAllTagsState = <(string | number)[]>[
