93c17cfe781a962facff606303ddd857b21ce529
achenson <45999022+Achenson@users.noreply.github.com>
2021-07-20 11:09:20 +0200

    GapAfterTab useTabs final

--- src/components/LowerUI/GapAfterTab.tsx
+++ src/components/LowerUI/GapAfterTab.tsx
@@ -7,6 +7,8 @@ import { ItemTypes } from "../../utils/itemsDnd";
 
 import { tabsDataState } from "../../state/tabsAndBookmarks";
 
+import {useTabs} from "../../state/useTabs"
+
 import { tabBeingDraggedColor_State } from "../../state/colorsState";
 import { globalSettingsState } from "../../state/defaultSettings";
 
@@ -33,9 +35,14 @@ function GapAfterTab({
   isThisLastGap,
   isThisTheOnlyGap,
 }: Props): JSX.Element {
-  const [tabsData, setTabsData] = tabsDataState.use();
+
+  // const [tabsData, setTabsData] = tabsDataState.use();
 
   const [globalSettingsData, setGlobalSettingsData] = globalSettingsState.use();
+
+
+  const tabs = useTabs(store => store.tabs);
+  const dragTab = useTabs(store => store.dragTab);
 
   const [tabBeingDraggedColor_Data, setTabBeingDraggedColor_Data] =
     tabBeingDraggedColor_State.use();
@@ -43,7 +50,7 @@ function GapAfterTab({
   const [{ isOver }, drop] = useDrop({
     //    required property
     accept: ItemTypes.BOOKMARK,
-    drop: (item: Item, monitor) => dragTab(item.tabID, item.colNumber),
+    drop: (item: Item, monitor) => dragTabWrapper(item.tabID, item.colNumber),
     // drop: (item, monitor) => console.log(item.tabID),
 
     collect: (monitor) => ({
@@ -51,114 +58,126 @@ function GapAfterTab({
     }),
   });
 
-  function dragTab(itemID: number | string, itemColNumber: number) {
-    setTabsData((previous) =>
-      produce(previous, (updated) => {
-        let itemToUpdate = updated.find((obj) => obj.id === itemID);
+  function dragTabWrapper(itemID: number | string, itemColNumber: number) {
 
-        if (itemToUpdate) {
-          itemToUpdate.column = colNumber;
-        }
 
-        // reseting priority numbers in column that was item origin
-        if (itemColNumber !== colNumber) {
-          tabsData
-            .filter((obj) => obj.column === itemColNumber)
-            .filter((obj) => obj.id !== itemID)
-            .sort((a, b) => a.priority - b.priority)
-            .forEach((filteredTab, i) => {
-              let tabToUpdate = updated.find(
-                (tab) => tab.id === filteredTab.id
-              );
-              if (tabToUpdate) {
-                tabToUpdate.priority = i;
-              }
-            });
-        }
 
-        // when column is empty
-        if (!tabID) {
-          if (itemToUpdate) {
-            itemToUpdate.priority = 0;
-          }
-          return;
-        }
+    dragTab(itemID, itemColNumber, colNumber, tabID);
 
-        // index of tab before the gap the tab is dragged into
-        let draggedIntoIndex: number = 0;
 
-        tabsData.forEach((obj, i) => {
-          if (obj.id === tabID) {
-            draggedIntoIndex = i;
-          }
-        });
+    // setTabsData((previous) =>
+    //   produce(previous, (updated) => {
+    //     let itemToUpdate = updated.find((obj) => obj.id === itemID);
 
-        let itemToUpdatePriority_initial = itemToUpdate?.priority as number;
+    //     if (itemToUpdate) {
+    //       itemToUpdate.column = colNumber;
+    //     }
 
-        let draggedIntoPriority = tabsData[draggedIntoIndex].priority;
+    //     // reseting priority numbers in column that was item origin
+    //     if (itemColNumber !== colNumber) {
+    //       tabs
+    //         .filter((obj) => obj.column === itemColNumber)
+    //         .filter((obj) => obj.id !== itemID)
+    //         .sort((a, b) => a.priority - b.priority)
+    //         .forEach((filteredTab, i) => {
+    //           let tabToUpdate = updated.find(
+    //             (tab) => tab.id === filteredTab.id
+    //           );
+    //           if (tabToUpdate) {
+    //             tabToUpdate.priority = i;
+    //           }
+    //         });
+    //     }
 
-        if (
-          itemID !== tabID &&
-          // if draggetItem do not belongs to the column OR draggedIntoTab is not the previous tab
-          (draggedIntoPriority + 1 !== itemToUpdate?.priority ||
-            colNumber !== itemColNumber)
-        ) {
-          if (itemToUpdate) {
-            if (colNumber !== itemColNumber) {
-              itemToUpdate.priority = draggedIntoPriority + 1;
-            } else {
-              // if dragging to a Tab further down
-              if (draggedIntoPriority > itemToUpdate.priority) {
-                itemToUpdate.priority = draggedIntoPriority;
-                //  if dragging to a Tab further up
-              } else {
-                itemToUpdate.priority = draggedIntoPriority + 1;
-              }
-            }
-          }
+    //     // when column is empty
+    //     if (!tabID) {
+    //       if (itemToUpdate) {
+    //         itemToUpdate.priority = 0;
+    //       }
+    //       return;
+    //     }
 
-          tabsData
-            .filter((obj) => obj.column === colNumber)
-            .filter((obj) => obj.id !== itemID)
-            .sort((a, b) => a.priority - b.priority)
-            .forEach((filteredTab, i) => {
-              let tabToUpdate = updated.find(
-                (tab) => tab.id === filteredTab.id
-              );
+    //     // index of tab before the gap the tab is dragged into
+    //     let draggedIntoIndex: number = 0;
 
-              if (tabToUpdate) {
-                if (tabToUpdate.priority <= draggedIntoPriority) {
-                  tabToUpdate.priority = i;
-                  // if tab being updated is further down that the tab being dragged INTO
-                } else {
-                  // updating priorities further down
+    //     tabs.forEach((obj, i) => {
+    //       if (obj.id === tabID) {
+    //         draggedIntoIndex = i;
+    //       }
+    //     });
 
-                  if (colNumber !== itemColNumber) {
-                    tabToUpdate.priority += 1;
-                  } else {
-                    // DO NOT UPDATE TABS further down then tab being dragged if tab is being dragged up
-                    if (
-                      draggedIntoPriority < itemToUpdatePriority_initial &&
-                      tabToUpdate.priority > itemToUpdatePriority_initial
-                    ) {
-                      return;
-                    }
-                    // DO NOT UPDATE TABS further down then tab being dragged INTO if tab is being dragged down
-                    if (
-                      draggedIntoPriority > itemToUpdatePriority_initial &&
-                      tabToUpdate.priority > draggedIntoPriority
-                    ) {
-                      return;
-                    }
+    //     let itemToUpdatePriority_initial = itemToUpdate?.priority as number;
 
-                    tabToUpdate.priority += 1;
-                  }
-                }
-              }
-            });
-        }
-      })
-    );
+    //     let draggedIntoPriority = tabs[draggedIntoIndex].priority;
+
+    //     if (
+    //       itemID !== tabID &&
+    //       // if draggetItem do not belongs to the column OR draggedIntoTab is not the previous tab
+    //       (draggedIntoPriority + 1 !== itemToUpdate?.priority ||
+    //         colNumber !== itemColNumber)
+    //     ) {
+    //       if (itemToUpdate) {
+    //         if (colNumber !== itemColNumber) {
+    //           itemToUpdate.priority = draggedIntoPriority + 1;
+    //         } else {
+    //           // if dragging to a Tab further down
+    //           if (draggedIntoPriority > itemToUpdate.priority) {
+    //             itemToUpdate.priority = draggedIntoPriority;
+    //             //  if dragging to a Tab further up
+    //           } else {
+    //             itemToUpdate.priority = draggedIntoPriority + 1;
+    //           }
+    //         }
+    //       }
+
+    //       tabs
+    //         .filter((obj) => obj.column === colNumber)
+    //         .filter((obj) => obj.id !== itemID)
+    //         .sort((a, b) => a.priority - b.priority)
+    //         .forEach((filteredTab, i) => {
+    //           let tabToUpdate = updated.find(
+    //             (tab) => tab.id === filteredTab.id
+    //           );
+
+    //           if (tabToUpdate) {
+    //             if (tabToUpdate.priority <= draggedIntoPriority) {
+    //               tabToUpdate.priority = i;
+    //               // if tab being updated is further down that the tab being dragged INTO
+    //             } else {
+    //               // updating priorities further down
+
+    //               if (colNumber !== itemColNumber) {
+    //                 tabToUpdate.priority += 1;
+    //               } else {
+    //                 // DO NOT UPDATE TABS further down then tab being dragged if tab is being dragged up
+    //                 if (
+    //                   draggedIntoPriority < itemToUpdatePriority_initial &&
+    //                   tabToUpdate.priority > itemToUpdatePriority_initial
+    //                 ) {
+    //                   return;
+    //                 }
+    //                 // DO NOT UPDATE TABS further down then tab being dragged INTO if tab is being dragged down
+    //                 if (
+    //                   draggedIntoPriority > itemToUpdatePriority_initial &&
+    //                   tabToUpdate.priority > draggedIntoPriority
+    //                 ) {
+    //                   return;
+    //                 }
+
+    //                 tabToUpdate.priority += 1;
+    //               }
+    //             }
+    //           }
+    //         });
+    //     }
+    //   })
+    // );
+
+
+
+
+
+
   }
 
   function calcOpacityOnDrop() {
--- src/state/useTabs.ts
+++ src/state/useTabs.ts
@@ -14,6 +14,12 @@ interface UseTabs {
   // deleting tab if there are no bookmarks with this tag (tab's name)
   deleteEmptyTab: (bookmarksAllTags: (string | number)[]) => void;
   deleteTab: (tabID: string | number) => void;
+  dragTab: (
+    itemID: number | string,
+    itemColNumber: number,
+    colNumber: number,
+    tabID: string | number | null
+  ) => void;
   editTab: (
     tabID: string | number,
     tabTitleInput: string,
@@ -30,7 +36,6 @@ interface UseTabs {
     setTabOpened_local: React.Dispatch<React.SetStateAction<boolean>>
 
     /* 
-    
     
        produce(previous, (updated) => {
         let tabToUpdate = updated.find((obj) => obj.id === tabID);
@@ -134,6 +139,115 @@ export const useTabs = create<UseTabs>((set) => ({
         if (tabToDelete) {
           let tabIndex = state.tabs.indexOf(tabToDelete);
           state.tabs.splice(tabIndex, 1);
+        }
+      })
+    ),
+
+  dragTab: (itemID, itemColNumber, colNumber, tabID) =>
+    set(
+      produce((state: UseTabs) => {
+        let itemToUpdate = state.tabs.find((obj) => obj.id === itemID);
+
+        if (itemToUpdate) {
+          itemToUpdate.column = colNumber;
+        }
+
+        // reseting priority numbers in column that was item origin
+        if (itemColNumber !== colNumber) {
+          state.tabs
+            .filter((obj) => obj.column === itemColNumber)
+            .filter((obj) => obj.id !== itemID)
+            .sort((a, b) => a.priority - b.priority)
+            .forEach((filteredTab, i) => {
+              let tabToUpdate = state.tabs.find(
+                (tab) => tab.id === filteredTab.id
+              );
+              if (tabToUpdate) {
+                tabToUpdate.priority = i;
+              }
+            });
+        }
+
+        // when column is empty
+        if (!tabID) {
+          if (itemToUpdate) {
+            itemToUpdate.priority = 0;
+          }
+          return;
+        }
+
+        // index of tab before the gap the tab is dragged into
+        let draggedIntoIndex: number = 0;
+
+        state.tabs.forEach((obj, i) => {
+          if (obj.id === tabID) {
+            draggedIntoIndex = i;
+          }
+        });
+
+        let itemToUpdatePriority_initial = itemToUpdate?.priority as number;
+
+        let draggedIntoPriority = state.tabs[draggedIntoIndex].priority;
+
+        if (
+          itemID !== tabID &&
+          // if draggetItem do not belongs to the column OR draggedIntoTab is not the previous tab
+          (draggedIntoPriority + 1 !== itemToUpdate?.priority ||
+            colNumber !== itemColNumber)
+        ) {
+          if (itemToUpdate) {
+            if (colNumber !== itemColNumber) {
+              itemToUpdate.priority = draggedIntoPriority + 1;
+            } else {
+              // if dragging to a Tab further down
+              if (draggedIntoPriority > itemToUpdate.priority) {
+                itemToUpdate.priority = draggedIntoPriority;
+                //  if dragging to a Tab further up
+              } else {
+                itemToUpdate.priority = draggedIntoPriority + 1;
+              }
+            }
+          }
+
+          state.tabs
+            .filter((obj) => obj.column === colNumber)
+            .filter((obj) => obj.id !== itemID)
+            .sort((a, b) => a.priority - b.priority)
+            .forEach((filteredTab, i) => {
+              let tabToUpdate = state.tabs.find(
+                (tab) => tab.id === filteredTab.id
+              );
+
+              if (tabToUpdate) {
+                if (tabToUpdate.priority <= draggedIntoPriority) {
+                  tabToUpdate.priority = i;
+                  // if tab being updated is further down that the tab being dragged INTO
+                } else {
+                  // updating priorities further down
+
+                  if (colNumber !== itemColNumber) {
+                    tabToUpdate.priority += 1;
+                  } else {
+                    // DO NOT UPDATE TABS further down then tab being dragged if tab is being dragged up
+                    if (
+                      draggedIntoPriority < itemToUpdatePriority_initial &&
+                      tabToUpdate.priority > itemToUpdatePriority_initial
+                    ) {
+                      return;
+                    }
+                    // DO NOT UPDATE TABS further down then tab being dragged INTO if tab is being dragged down
+                    if (
+                      draggedIntoPriority > itemToUpdatePriority_initial &&
+                      tabToUpdate.priority > draggedIntoPriority
+                    ) {
+                      return;
+                    }
+
+                    tabToUpdate.priority += 1;
+                  }
+                }
+              }
+            });
         }
       })
     ),
